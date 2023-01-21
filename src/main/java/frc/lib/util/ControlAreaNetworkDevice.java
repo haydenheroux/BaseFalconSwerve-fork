b378b6addc41334789a9a8ff4e3559989844b129
Hayden Heroux <haydenheroux@gmail.com>
2023-01-21 17:46:54 -0500

    Format code using Spotless.

--- src/main/java/frc/lib/util/ControlAreaNetworkDevice.java
+++ src/main/java/frc/lib/util/ControlAreaNetworkDevice.java
@@ -1,18 +1,17 @@
 package frc.lib.util;
 
 public class ControlAreaNetworkDevice {
-    
-    public final int id;
-    public final String bus;
 
-    public ControlAreaNetworkDevice(int id) {
-        this.id = id;
-        this.bus = "";
-    }
+  public final int id;
+  public final String bus;
 
-    public ControlAreaNetworkDevice(int id, String bus) {
-        this.id = id;
-        this.bus = bus;
-    }
+  public ControlAreaNetworkDevice(int id) {
+    this.id = id;
+    this.bus = "";
+  }
 
+  public ControlAreaNetworkDevice(int id, String bus) {
+    this.id = id;
+    this.bus = bus;
+  }
 }
--- src/main/java/frc/lib/util/SwerveModuleConstants.java
+++ src/main/java/frc/lib/util/SwerveModuleConstants.java
@@ -3,22 +3,27 @@ package frc.lib.util;
 import edu.wpi.first.math.geometry.Rotation2d;
 
 public class SwerveModuleConstants {
-    public final ControlAreaNetworkDevice driveMotor;
-    public final ControlAreaNetworkDevice angleMotor;
-    public final ControlAreaNetworkDevice encoder;
-    public final Rotation2d angleOffset;
+  public final ControlAreaNetworkDevice driveMotor;
+  public final ControlAreaNetworkDevice angleMotor;
+  public final ControlAreaNetworkDevice encoder;
+  public final Rotation2d angleOffset;
 
-    /**
-     * Swerve Module Constants to be used when creating swerve modules.
-     * @param driveMotorID
-     * @param angleMotorID
-     * @param canCoderID
-     * @param angleOffset
-     */
-    public SwerveModuleConstants(ControlAreaNetworkDevice driveMotor, ControlAreaNetworkDevice angleMotor, ControlAreaNetworkDevice encoder, Rotation2d angleOffset) {
-        this.driveMotor = driveMotor;
-        this.angleMotor = angleMotor;
-        this.encoder = encoder;
-        this.angleOffset = angleOffset;
-    }
+  /**
+   * Swerve Module Constants to be used when creating swerve modules.
+   *
+   * @param driveMotorID
+   * @param angleMotorID
+   * @param canCoderID
+   * @param angleOffset
+   */
+  public SwerveModuleConstants(
+      ControlAreaNetworkDevice driveMotor,
+      ControlAreaNetworkDevice angleMotor,
+      ControlAreaNetworkDevice encoder,
+      Rotation2d angleOffset) {
+    this.driveMotor = driveMotor;
+    this.angleMotor = angleMotor;
+    this.encoder = encoder;
+    this.angleOffset = angleOffset;
+  }
 }
